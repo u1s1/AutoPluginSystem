b525b4f83b54466781d59227bce6ed1c28b7cafb
zdp <zdpoffice@qq.com>
2026-03-10 15:25:59 +0800

    test: 添加测试代码;

--- AutoPluginSystem/src/AutoPlugin.cpp
+++ AutoPluginSystem/src/AutoPlugin.cpp
@@ -4,10 +4,15 @@ int main()
 {
     std::cout << "Registered APIs count: " << GetApiRegistry().size() << std::endl;
     PluginManager manager;
-    if (manager.LoadAndStart("D:\\git\\AutoPluginSystem\\test\\Customer\\build\\Release\\MyPlugin.dll")) {
+    if (manager.Install("D:\\git\\AutoPluginSystem\\test\\Customer\\build\\Release\\MyPlugin.dll")) {
         std::cout << "Plugin loaded and started successfully!" << std::endl;
     } else {
         std::cout << "Failed to load or start plugin." << std::endl;
+    }
+    if (manager.Start("MyPlugin")) {
+        std::cout << "Plugin  started successfully!" << std::endl;
+    } else {
+        std::cout << "Failed to start plugin." << std::endl;
     }
     manager.Stop();
     std::cout << std::endl;
